dfc6cd576a82f2d8d548f228dcc4f0cf4c8bd61a
HAFDIAHMED <ahafdi62@gmail.com>
2021-10-01 09:10:53 +0100

    first test failed

---
+++ testing/jest-setup.js
@@ -0,0 +1,9 @@
+import 'react-native-gesture-handler/jestSetup';
+
+jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
+
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+  Reanimated.default.call = () => {};
+  return Reanimated;
+});
